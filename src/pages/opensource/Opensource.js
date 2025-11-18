05939683dcb704b3efc33273fea62bc3bcbc0bc1
husnizayyin <husni.zayyin98@gmail.com>
2025-11-18 19:24:17 +0700

    fear : remove footer

--- src/pages/opensource/Opensource.js
+++ src/pages/opensource/Opensource.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Header from "../../components/header/Header";
-import Footer from "../../components/footer/Footer";
 import OpensourceCharts from "../../containers/opensourceCharts/OpensourceCharts";
 import Organizations from "../../containers/organizations/Organizations";
 import PullRequests from "../../containers/pullRequests/PullRequests";
